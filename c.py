7fbe7152ae08b7eee0755075ae4cd3fdd3bf61ed
Sudeepthi Rao <sudeepthi.rao@example.com>
2026-09-15 14:52:13 +0530

    Add multiplication feature

--- c.py
+++ c.py
@@ -1,4 +1,16 @@
-a=3
+a=int(input("enter a number:"))
 b=4
 c=a*b
 print(c)
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
